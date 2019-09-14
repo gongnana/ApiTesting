8c1eb5a340775bec4ad138ca4eda68616afb8332
gongnana <eunice_gong@163.com>
2019-09-14 18:06:16 +0800

    init

---
+++ testcase/test_requests.py
@@ -0,0 +1,18 @@
+import logging
+import requests
+import pytest
+import json
+
+
+class TestRequests(object):
+    logging.basicConfig(level=logging.INFO)
+
+    def test_get(self):
+        r = requests.get('https://testerhome.com/api/v3/topics.json')
+        logging.info(r)
+        logging.info(r.text)
+        logging.info(json.dumps(r.json(), indent=2))
+
+
+if __name__ == '__main__':
+    pytest.main(['test_requests'])
